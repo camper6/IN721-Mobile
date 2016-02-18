19960fa34e9bd0ff5cfd0a5c17d4cdc840d31947
camper6 <sonshu.soush@gmail.com>
2016-02-18 16:12:26 +1300

    First App
    Practical 1.1

---
+++ MyFirstApplication/app/src/main/java/bit/abduhs1/myfirstapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package bit.abduhs1.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
