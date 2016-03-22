706b6c395dcb9445640dda22c42e9ab84672b77f
camper6 <sonshu.soush@gmail.com>
2016-03-22 17:52:20 +1300

    Working App

--- MusicSchool/app/src/main/java/bit/abduhs1/musicschool/MainActivity.java
+++ MusicSchool/app/src/main/java/bit/abduhs1/musicschool/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,23 +17,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner months = (Spinner) findViewById(R.id.months);
+        Button btnEnrol = (Button) findViewById(R.id.btEnrol);
+        btnEnrol.setOnClickListener(new OnClickHandler());
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.months_array, android.R.layout.simple_spinner_item);
+        Spinner spnMonths = (Spinner) findViewById(R.id.spnMonths);
 
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, R.array.months);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.months, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnMonths.setAdapter(adapter);
 
-        months.setAdapter(adapter);
     }
 
-    public class EventHandler implements View.OnClickListener {
+    public class OnClickHandler implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
+            RadioButton rdoAccordion = (RadioButton) findViewById(R.id.radioButtonAccordian);
+            RadioButton rdoBassoon = (RadioButton) findViewById(R.id.radioButtonBass);
+            RadioButton rdoCello = (RadioButton) findViewById(R.id.radioButtonCello);
 
+            TextView textViewConfirmed = (TextView) findViewById(R.id.textViewConfirmed);
 
+            Spinner spnMonths = (Spinner) findViewById(R.id.spnMonths);
+            String selectedMonth = "";
 
+            if (rdoAccordion.isChecked()){
+                selectedMonth = (String) spnMonths.getSelectedItem();
+                textViewConfirmed.setText("You are now enrolled for Accordion lessons in " + selectedMonth + ".");
+            }else if(rdoBassoon.isChecked()){
+                selectedMonth = (String) spnMonths.getSelectedItem();
+                textViewConfirmed.setText("You are now enrolled for Bassoon lessons in " + selectedMonth + ".");
+            }else if(rdoCello.isChecked()){
+                selectedMonth = (String) spnMonths.getSelectedItem();
+                textViewConfirmed.setText("You are now enrolled for Cello lessons in " + selectedMonth + ".");
+            }else{
+                Toast.makeText(MainActivity.this, "You have not selected an instrument.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
