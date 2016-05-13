aa207fa2005fa43179055dd26480a0ce4c16ae87
camper6 <sonshu.soush@gmail.com>
2016-05-13 14:56:57 +1200

    RESULT CODE, NOT REQUEST CODE. Image too big

--- PhotoMosaic/app/src/main/java/bit/abduhs1/photomosaic/MainActivity.java
+++ PhotoMosaic/app/src/main/java/bit/abduhs1/photomosaic/MainActivity.java
@@ -63,9 +63,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1) {
-            if (requestCode == RESULT_OK) {
+            if (resultCode == RESULT_OK) {
                 String readFilePath = mPhotoFile.getPath();
                 Bitmap userPhotoBitmap = BitmapFactory.decodeFile(readFilePath);
+                userPhotoBitmap = Bitmap.createScaledBitmap(userPhotoBitmap, 300, 300, false);
 
                 imageView.setImageBitmap(userPhotoBitmap);
                 imageView2.setImageBitmap(userPhotoBitmap);
